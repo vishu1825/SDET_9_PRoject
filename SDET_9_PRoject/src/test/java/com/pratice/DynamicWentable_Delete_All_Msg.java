8979e4798080c910e6c84cd16cb3dbc883a4caa5
Deepak <Deepak@Ty-Deepak-Tr>
2021-01-23 13:31:46 +0530

    push

---
+++ SDET_9_PRoject/src/test/java/com/pratice/DynamicWentable_Delete_All_Msg.java
@@ -0,0 +1,45 @@
+package com.pratice;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class DynamicWentable_Delete_All_Msg {
+
+	public static void main(String[] args) {
+		
+		WebDriver driver = new FirefoxDriver();
+		driver.manage().timeouts().implicitlyWait(20, TimeUnit.MICROSECONDS);
+		
+		driver.get("http://localhost:8888");
+          //login
+		  driver.findElement(By.name("user_name")).sendKeys("admin");
+		  driver.findElement(By.name("user_password")).sendKeys("admin");
+		  driver.findElement(By.id("submitButton")).click();
+		  
+		  //navigate to Organizations Page
+		  driver.findElement(By.linkText("Organizations")).click();
+		  
+		  WebDriverWait wait = new WebDriverWait(driver, 20);
+		  wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//table[@class='lvt small']/tbody/tr[*]/td[3]/a")));
+		  
+		  
+		  //capture all the Organizations name aviable in dynamic webtables & display in console
+		  List<WebElement> lst = driver.findElements(By.xpath("//table[@class='lvt small']/tbody/tr[*]/td[1]/input[@name='selected_id']"));
+		  
+		  for(WebElement wb : lst) {
+			  wb.click();
+		  }
+		
+
+	}
+
+}
